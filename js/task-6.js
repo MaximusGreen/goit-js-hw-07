d8e522a08fc625079ab4c7177e87224b56b918ca
MaximusGreen <154276386+MaximusGreen@users.noreply.github.com>
2024-04-27 21:18:43 +0300

    1

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createBtn.addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount < 1 || amount > 100) {
+    return;
+  }
+  createBoxes(amount);
+  input.value = '';
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  destroyBoxes();
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+
+
